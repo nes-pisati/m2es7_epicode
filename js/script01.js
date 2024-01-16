ecab8d7d6fd257f2579235ddc988b1029875ec54
Vanessa Pisati <pisati.vanessa@gmail.com>
2024-01-16 13:26:20 +0100

    Nuovo commit

--- js/script01.js
+++ js/script01.js
@@ -1,18 +1,27 @@
-const jobTitle = document.querySelector("#jobTitle");
-const jobLocation = document.querySelector("#location");
-
-console.log(jobTitle, jobLocation);
-
 function searchJob (jobTitle, jobLocation) {
-    let title = jobTitle.value.tolowercase();
-    let location = jobLocation.value.tolowercase();
-
-    
+    let title = jobTitle.tolowercase();
+    let location = jobLocation.tolowercase();
+    let result = [];
+    let count = 0;
 
     jobs.forEach(job => {
         if (jobs.location.includes(location) && jobs.title.includes(title)) {
-            
+            result.push(job);
         }
     })
 
+    return {result,
+    count: result.length}
 }
+
+searchJob()
+
+console.log(searchJob(developer, US));
+
+//console.log(searchJob(developer, NY));
+
+/*const jobTitle = document.querySelector("#jobTitle");
+const jobLocation = document.querySelector("#location");
+const searchBtn = document.querySelector("#searchBtn");
+
+console.log(jobTitle, jobLocation, searchBtn);*/
