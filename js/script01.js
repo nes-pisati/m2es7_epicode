10b423fa7ea5e3d2278c16e9bcead3f459ccda7f
Vanessa Pisati <pisati.vanessa@gmail.com>
2024-01-17 23:47:46 +0100

    ultimo commit

--- js/script01.js
+++ js/script01.js
@@ -1,6 +1,7 @@
-const result = [];
+let result = [];
+let count = 0; 
 
-function searchJob (title, location) {
+function searchJob (title, location) { 
 
     title = title.toLowerCase();
     location = location.toLowerCase();
@@ -14,41 +15,66 @@ function searchJob (title, location) {
         }
     })
 
+    count = result.length;
+
     return {
-        count: result.length,
+        count,
         result
     }
 }
 
-//console.log(searchJob("marketing","NY"))
-//console.log(result);
+//console.log(searchJob("marketing","US"))
 
 function findJob() {
     const jobTitle = document.querySelector("#jobTitle").value;
     const jobLocation = document.querySelector("#jobLocation").value;
 
-    const results = searchJob(jobTitle, jobLocation);
+    if ((jobTitle != "")&&(jobLocation != "")){
+    let results = searchJob(jobTitle, jobLocation);
 
-    return results
+    const resultsDiv = document.getElementById("results");
+    const resultTitle = document.createElement("h2");
+    //console.log(results);
+
+    if (result.length>=1){
+    
+    resultTitle.innerHTML = "Ecco i risultati della tua ricerca del ruolo " + jobTitle + " nella località " + jobLocation;
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("cardContainer");
+    resultsDiv.append(resultTitle, cardContainer);
+
+    for (let i=0; i <result.length; i++) {
+        const card = document.createElement("div");
+        card.classList.add("jobCard");
+        const title = document.createElement("p");
+        title.classList.add("jobTitleField");
+        title.innerHTML = "<span>Job Title: </span><br>" + result[i].title;
+        const location = document.createElement("p");
+        location.classList.add("jobLocationField");
+        location.innerHTML = "<span>Job Location: </span><br>" + result[i].location;
+
+        card.append(title, location)
+        cardContainer.append(card);
+    }} else {
+        resultTitle.innerHTML = "Non ci sono posizioni corrispondenti alla tua richiesta";
+        resultsDiv.append(resultTitle);
+    }} 
+    
+    else {
+        alert("Per favore compila i campi richiesti")
+    }
 }
+
 
 const button = document.querySelector("form");
 button.addEventListener("submit", (e)=> {
     e.preventDefault();
-    showJobs();
+    findJob();
+    cleanInput();
 })
 
-function showJobs() {
-    const resultsDiv = document.getElementById("results");
-    const resultTitle = document.createElement("h2");
-    resultTitle.textContent = "Ecco i risultati della tua ricerca:"
-
-    /*result.forEach((element) => {
-        const card = document.createElement("div");
-        card.classList.add("jobCard");
-        const title = document.createElement("p");
-        title.textContent = "ciao"
-    })*/
-
-    resultsDiv.append(resultTitle);
-}; 
+function cleanInput(){
+    document.querySelector("#jobTitle").value = "";
+    document.querySelector("#jobLocation").value = "";
+    result = [];
+}
